cb4854052d1fe6f5ca9f7aecff2766ae9b8edb6a
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-11 06:05:51 +0900

    update

--- mnist_main.py
+++ mnist_main.py
@@ -1,11 +1,5 @@
-"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import numpy as np
 import tensorflow as tf
+import numpy as np
 import model
 import opt
 
